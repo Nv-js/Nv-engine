94a7d1ea0ee49ee3e02caeb74ff8381ead1dff86
zhangjinglin <zhangjinglin@jd.com>
2019-09-03 18:04:31 +0800

    modify a serious bug
    when many same name file path to loaded in the same time, the error be warning. (Fixed)

--- src/index.js
+++ src/index.js
@@ -7,33 +7,41 @@ import {global,log,throwError,cmpstaus}    from './config/global'
 
 
 let _T = {
-      document:window.document,
-      window:window,
-      tools,
-      request:(url, callback) => {
-            const _f = new file(url, callback);
-            _f.request();
-      },
-      add:module.add,
-      use:module.use,
-      config:module.config,
-      baseConfig:global.CONFIG,
-      require:getFile,
-      extend:tools.extend,
-      global,
-      log,
-      throwError,
-      cmpstaus,
-      _pub:new publish(),
-      readyQueen:[],
-      ready:function(fn){
-            _T.readyQueen.push(fn)
-      }     
+    document:window.document,
+    window:window,
+    tools,
+    request:(url, callback) => {
+        const _f = new file(url, callback);
+        _f.request();
+    },
+    add:module.add,
+    use:module.use,
+    config:module.config,
+    baseConfig:global.CONFIG,
+    require:getFile,
+    extend:tools.extend,
+    global,
+    log,
+    throwError,
+    cmpstaus,
+    _pub:new publish(),
+    readyQueen:[],
+    ready:function(fn){
+        _T.readyQueen.push(fn)
+    }
 }
 
 
 if(!window[global.PREFIX]){
-      //发布订阅初始化
-      window[global.PREFIX] = _T
+    //发布订阅初始化
+    window[global.PREFIX] = _T
+    window[global.PREFIX].tools.later(function(){
+        window[global.PREFIX].use('init',function(){
+            tools.each(window[global.PREFIX].readyQueen,function (index,ele) {
+                ele()
+            })
+        })
+    },0)
+
 }
 
